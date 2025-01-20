e90c3fc84620fc7125db962ff6a3c49274ee9f01
tamdaz <tamda.zohir.pro@gmail.com>
2025-01-20 22:05:12 +0100

    Improve annotation spec.

--- spec/annotation_spec.cr
+++ spec/annotation_spec.cr
@@ -9,7 +9,7 @@ describe Crygen::Types::Enum do
     CRYSTAL
   end
 
-  it "creates an annotation with one parameter" do
+  it "creates an annotation with one parameter (value only)" do
     annotation_type = Crygen::Types::Annotation.new("MyAnnotation")
     annotation_type.add_arg("true")
     annotation_type.generate.should eq(<<-CRYSTAL)
@@ -17,13 +17,31 @@ describe Crygen::Types::Enum do
     CRYSTAL
   end
 
-  it "creates an annotation with many parameters" do
+  it "creates an annotation with one parameter (name and value)" do
+    annotation_type = Crygen::Types::Annotation.new("MyAnnotation")
+    annotation_type.add_arg("is_cool", "true")
+    annotation_type.generate.should eq(<<-CRYSTAL)
+    @[MyAnnotation(is_cool: true)]
+    CRYSTAL
+  end
+
+  it "creates an annotation with many parameters (values only)" do
     annotation_type = Crygen::Types::Annotation.new("MyAnnotation")
     annotation_type.add_arg("true")
     annotation_type.add_arg("1")
     annotation_type.add_arg("Hello World".dump)
     annotation_type.generate.should eq(<<-CRYSTAL)
     @[MyAnnotation(true, 1, "Hello World")]
+    CRYSTAL
+  end
+
+  it "creates an annotation with many parameters (name and value)" do
+    annotation_type = Crygen::Types::Annotation.new("MyAnnotation")
+    annotation_type.add_arg("is_cool", "true")
+    annotation_type.add_arg("number", "1")
+    annotation_type.add_arg("text", "Hello World".dump)
+    annotation_type.generate.should eq(<<-CRYSTAL)
+    @[MyAnnotation(is_cool: true, number: 1, text: "Hello World")]
     CRYSTAL
   end
 end
