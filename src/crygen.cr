2b548de56d26f3c70d63189c5f0a90d2abd0ee4c
tamdaz <tamda.zohir.pro@gmail.com>
2025-01-20 22:50:41 +0100

    Add comments for aliases and `Crygen` module.

--- src/crygen.cr
+++ src/crygen.cr
@@ -1,9 +1,12 @@
 require "./types/**"
 require "./modules/**"
 
+# **crygen** is a library that allows to generate a Crystal file. It is inspired by the PHP
+# library : [nette/php-generator](https://github.com/nette/php-generator).
 module Crygen
+  # Library version
   VERSION = "0.1.0"
 
-  alias ::CGM = Crygen::Modules
+  # CGT is a alias of "**C**ry**G**en **T**ypes", it simplifies the name.
   alias ::CGT = Crygen::Types
 end
