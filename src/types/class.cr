a73d8c102f021227f624d12d546ab8a873ea5477
tamdaz <tamda.zohir.pro@gmail.com>
2025-01-24 23:32:37 +0100

    Delete the `class_type` private method.

--- src/types/class.cr
+++ src/types/class.cr
@@ -56,7 +56,11 @@ class Crygen::Types::Class < Crygen::Abstract::GeneratorInterface
     String.build do |str|
       @comments.each { |comment| str << "# #{comment}\n" }
       @annotations.each { |annotation_type| str << annotation_type.generate + "\n" }
-      str << class_type
+      str << if @type == :abstract
+        "abstract class #{@name}\n"
+      else
+        "class #{@name}\n"
+      end
       generate_instance_vars.each_line { |line| str << "  " + line + "\n" }
       generate_class_vars.each_line { |line| str << "  " + line + "\n" }
       can_add_whitespace = false
@@ -71,15 +75,6 @@ class Crygen::Types::Class < Crygen::Abstract::GeneratorInterface
         end
       end
       str << "end"
-    end
-  end
-
-  # Defines a class type.
-  private def class_type : String
-    if @type == :abstract
-      "abstract class #{@name}\n"
-    else
-      "class #{@name}\n"
     end
   end
 end
