396dd1f5752b8e32d5e89faa43f836e8425917c5
Zohir Tamda <120991635+tamdaz@users.noreply.github.com>
2025-03-12 09:44:11 +0100

    Improve alias spec. (#22)

--- spec/alias_spec.cr
+++ spec/alias_spec.cr
@@ -2,8 +2,18 @@ require "./spec_helper"
 
 describe Crygen::Types::Alias do
   it "creates an alias" do
+    Crygen::Types::Alias.new("MyAlias", ["Foo"]).generate.should eq(<<-CRYSTAL)
+    alias MyAlias = Foo
+    CRYSTAL
+  end
+
+  it "creates an alias with unions" do
     Crygen::Types::Alias.new("MyAlias", %w[Foo Bar]).generate.should eq(<<-CRYSTAL)
     alias MyAlias = Foo | Bar
+    CRYSTAL
+
+    Crygen::Types::Alias.new("MyAlias", %w[A::Foo B::Foo C::Bar D::Bar]).generate.should eq(<<-CRYSTAL)
+    alias MyAlias = A::Foo | B::Foo | C::Bar | D::Bar
     CRYSTAL
   end
 end
