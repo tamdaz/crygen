83fb92bd1f842053bcffdb03dbc5b276487fe510
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-03-12 15:21:35 +0100

    Add docs for alias.

--- src/types/alias.cr
+++ src/types/alias.cr
@@ -1,11 +1,21 @@
 require "./../modules/*"
 require "./../interfaces/generator_interface"
 
+# Class that is used to generate the aliases.
+# ```
+# alias_type = CGT::Alias.new("MyAlias", %w[Foo Bar])
+# puts alias_type.generate
+# ```
+# Output:
+# ```
+# alias MyAlias = Foo | Bar
+# ```
 class Crygen::Types::Alias < Crygen::Abstract::GeneratorInterface
   include Crygen::Modules::Comment
 
   def initialize(@name : String, @types : Array(String)); end
 
+  # Generates an alias.
   def generate : String
     String.build do |str|
       @comments.each { |comment| str << "# #{comment}\n" }
