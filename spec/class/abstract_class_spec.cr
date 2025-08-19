799daa7e0d5130581333339903c542f0e0f65dc4
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-08-19 22:24:15 +0200

    Update the spec: abstract_class_spec.cr

--- spec/class/abstract_class_spec.cr
+++ spec/class/abstract_class_spec.cr
@@ -22,7 +22,7 @@ describe Crygen::Types::Class do
 
       class_type = test_person_class()
       class_type.as_abstract
-      class_type.add_method(CGT::Method.new("full_name", "String"))
+      class_type.add_method(CGT::Method.new("full_name", "String").as_abstract)
       class_type.generate.should eq(expected)
       class_type.to_s.should eq(expected)
     end
@@ -39,9 +39,30 @@ describe Crygen::Types::Class do
       class_type = test_person_class()
       class_type.as_abstract
       class_type.add_methods(
-        CGT::Method.new("first_name", "String"),
-        CGT::Method.new("last_name", "String"),
-        CGT::Method.new("full_name", "String")
+        CGT::Method.new("first_name", "String").as_abstract,
+        CGT::Method.new("last_name", "String").as_abstract,
+        CGT::Method.new("full_name", "String").as_abstract
+      )
+      class_type.generate.should eq(expected)
+      class_type.to_s.should eq(expected)
+    end
+
+    it "creates an abstract class with abstract and non-abstract methods" do
+      expected = <<-CRYSTAL
+      abstract class Person
+        abstract def abstract_method : String
+
+        def non_abstract_method : String
+          "Hello world"
+        end
+      end
+      CRYSTAL
+
+      class_type = test_person_class()
+      class_type.as_abstract
+      class_type.add_methods(
+        CGT::Method.new("abstract_method", "String").as_abstract,
+        CGT::Method.new("non_abstract_method", "String").add_body("Hello world".dump)
       )
       class_type.generate.should eq(expected)
       class_type.to_s.should eq(expected)
