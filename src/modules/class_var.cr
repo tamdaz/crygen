386f6c947283b8b40776ffc1b22c39a649d29545
Zohir Tamda <120991635+tamdaz@users.noreply.github.com>
2025-10-14 23:21:02 +0200

    Possibility to add annotations to instance vars and class vars (#64)
    * Possibility to add annotations to instance vars.
    
    * Add specs
    
    * Calling `#to_s` is not necessary for generating the annotation.
    
    * Trigger `docs` workflow if the branch name starts with `docs/`.
    
    * Possibility to add annotations to class vars.
    
    * Add specs for the structs.
    
    * Avoid the code repetition.
    
    * Add the documentation on `Crygen::Modules::InstanceVar#add_instance_var`

--- src/modules/class_var.cr
+++ src/modules/class_var.cr
@@ -1,24 +1,43 @@
+require "./../types/annotation"
+
 # Module that is used to store class variables.
 module Crygen::Modules::ClassVar
-  @class_vars = [] of Tuple(String, String, String?)
+  @class_vars = [] of Tuple(String, String, String?, Array(Crygen::Types::Annotation)?)
 
   # Adds a class var with default value.
-  def add_class_var(name : String, type : String, value : String? = nil) : self
+  def add_class_var(
+    name : String, type : String, value : String? = nil,
+    annotations : Array(Crygen::Types::Annotation)? = nil,
+  ) : self
     output_value = if type == "String" && !value.nil?
                      value.dump
                    else
                      value
                    end
 
-    @class_vars << {name, type, output_value}
+    @class_vars << {name, type, output_value, annotations}
     self
+  end
+
+  # Adds a class var with default value.
+  def add_class_var(
+    name : String, type : String, value : String? = nil,
+    the_annotation : Crygen::Types::Annotation? = nil,
+  ) : self
+    add_class_var(name, type, value, [the_annotation])
   end
 
   # Generate the class_vars.
   def generate_class_vars : String
     String.build do |str|
       @class_vars.each do |class_var|
-        name, type, value = class_var
+        name, type, value, annotations = class_var
+
+        if annotations
+          annotations.each do |ann|
+            str << ann << "\n"
+          end
+        end
 
         str << "@@" << name << " : " << type
         str << " = " << value unless value.nil?
--- src/modules/instance_var.cr
+++ src/modules/instance_var.cr
@@ -1,23 +1,43 @@
+require "./../types/annotation"
+
 # Module that is used to store and add the instance variables.
 module Crygen::Modules::InstanceVar
-  @instance_vars = [] of Tuple(String, String, String?)
+  @instance_vars = [] of Tuple(String, String, String?, Array(Crygen::Types::Annotation)?)
 
   # Adds an argument with default value.
-  def add_instance_var(name : String, type : String, value : String? = nil) : self
+  def add_instance_var(
+    name : String, type : String, value : String? = nil,
+    annotations : Array(Crygen::Types::Annotation)? = nil,
+  ) : self
     output_value = if type == "String" && !value.nil?
                      value.dump
                    else
                      value
                    end
-    @instance_vars << {name, type, output_value}
+
+    @instance_vars << {name, type, output_value, annotations}
     self
+  end
+
+  # Adds an argument with default value.
+  def add_instance_var(
+    name : String, type : String, value : String? = nil,
+    the_annotation : Crygen::Types::Annotation? = nil,
+  ) : self
+    add_instance_var(name, type, value, [the_annotation])
   end
 
   # Generate the instance vars.
   def generate_instance_vars : String
     String.build do |str|
       @instance_vars.each do |instance_var|
-        name, type, value = instance_var
+        name, type, value, annotations = instance_var
+
+        if annotations
+          annotations.each do |ann|
+            str << ann << "\n"
+          end
+        end
 
         str << '@' << name << " : " << type
         str << " = " << value unless value.nil?
