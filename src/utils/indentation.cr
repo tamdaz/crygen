59de1ac0356bf34aea199d9e196fc107f9483625
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-11-06 16:25:30 +0100

    Add a few static methods for resetting the indentations.

--- src/utils/indentation.cr
+++ src/utils/indentation.cr
@@ -1,7 +1,9 @@
 # The static class that enables to control the indentation of the code. This is useful when we
 # create the nested objects (for example: class in a class and struct in a struct).
+# TODO: Document this static class.
 class Crygen::Utils::Indentation
   @@number_of_indents : Int32 = 0
+  @@saved_number : Int32 = 0
 
   # Add indentation
   def self.add_indent(size : Number = 2) : Void
@@ -13,7 +15,18 @@ class Crygen::Utils::Indentation
   end
 
   def self.reset : Void
+    @@saved_number = @@number_of_indents
     @@number_of_indents = 0
+  end
+
+  def self.complete_reset : Void
+    @@number_of_indents = 0
+    @@saved_number = 0
+  end
+
+  def self.restore : Void
+    @@number_of_indents = @@saved_number
+    @@saved_number = 0
   end
 
   def self.generate : String
