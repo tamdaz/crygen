a1debfaf5032b7ec6a74187b39a39c8952fa1587
Zohir Tamda <120991635+tamdaz@users.noreply.github.com>
2025-11-07 10:33:59 +0100

    Write the documentation to `CGG::*` static classes (#84)
    * Write documentation to `Crygen::Generators::Annotation` static class.
    
    * Write documentation to `Crygen::Generators::Comment` static class.

--- src/generators/comment.cr
+++ src/generators/comment.cr
@@ -1,4 +1,6 @@
+# Static class generator that allows to generate the multiline comment(s).
 class Crygen::Generators::Comment
+  # Generates the n line comment.
   def self.generate(comments : Array(String)) : String
     String.build do |str|
       comments.each do |comment|
