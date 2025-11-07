9030b0a94011b2332000b879bcafe65f08d6c8d2
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-11-07 02:01:47 +0100

    Write documentation for the `Crygen::Utils::Indentation` static class.

--- src/utils/indentation.cr
+++ src/utils/indentation.cr
@@ -1,34 +1,41 @@
 # The static class that enables to control the indentation of the code. This is useful when we
-# create the nested objects (for example: class in a class and struct in a struct).
-# TODO: Document this static class.
+# create the nested objects (for example: nested classes and structs).
 class Crygen::Utils::Indentation
+  # Number of spaces to indent
   @@number_of_spaces : Int32 = 0
+
+  # Used to save the number of spaces after reset.
   @@saved_number : Int32 = 0
 
-  # Add indentation
+  # Adds the indentation with the defined size.
   def self.add_indent(size : Number = 2) : Void
     @@number_of_spaces += size
   end
 
+  # Removes the indentation with the defined size.
   def self.remove_indent(size : Number = 2) : Void
     @@number_of_spaces -= size
   end
 
+  # Resets temporarily the indentation to zero but keep the number for restoring.
   def self.reset : Void
     @@saved_number = @@number_of_spaces
     @@number_of_spaces = 0
   end
 
+  # Resets the indentation to zero without restoration.
   def self.complete_reset : Void
     @@number_of_spaces = 0
     @@saved_number = 0
   end
 
+  # Restores the number of spaces for indentation.
   def self.restore : Void
     @@number_of_spaces = @@saved_number
     @@saved_number = 0
   end
 
+  # Generate the indentation.
   def self.generate : String
     String.build do |str|
       @@number_of_spaces.times { str << ' ' }
