f8a2ecc476e7232b96c579639f7fc510e0379e63
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-11-07 01:59:16 +0100

    Rename the class var to @@number_of_spaces

--- src/utils/indentation.cr
+++ src/utils/indentation.cr
@@ -2,36 +2,36 @@
 # create the nested objects (for example: class in a class and struct in a struct).
 # TODO: Document this static class.
 class Crygen::Utils::Indentation
-  @@number_of_indents : Int32 = 0
+  @@number_of_spaces : Int32 = 0
   @@saved_number : Int32 = 0
 
   # Add indentation
   def self.add_indent(size : Number = 2) : Void
-    @@number_of_indents += size
+    @@number_of_spaces += size
   end
 
   def self.remove_indent(size : Number = 2) : Void
-    @@number_of_indents -= size
+    @@number_of_spaces -= size
   end
 
   def self.reset : Void
-    @@saved_number = @@number_of_indents
-    @@number_of_indents = 0
+    @@saved_number = @@number_of_spaces
+    @@number_of_spaces = 0
   end
 
   def self.complete_reset : Void
-    @@number_of_indents = 0
+    @@number_of_spaces = 0
     @@saved_number = 0
   end
 
   def self.restore : Void
-    @@number_of_indents = @@saved_number
+    @@number_of_spaces = @@saved_number
     @@saved_number = 0
   end
 
   def self.generate : String
     String.build do |str|
-      @@number_of_indents.times { str << ' ' }
+      @@number_of_spaces.times { str << ' ' }
     end
   end
 end
