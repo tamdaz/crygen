5520ef15e5c4a4ae1b21460cb82042af010fc5fd
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-11-08 17:46:39 +0100

    Update the spec : Crygen::Types::Alias

--- spec/alias_spec.cr
+++ spec/alias_spec.cr
@@ -2,18 +2,42 @@ require "./spec_helper"
 
 describe Crygen::Types::Alias do
   it "creates an alias" do
+    expected = <<-CRYSTAL
+    alias MyAlias = Foo
+    CRYSTAL
+
     alias_type = Crygen::Types::Alias.new("MyAlias", ["Foo"])
-    alias_type.generate.should eq("alias MyAlias = Foo")
-    alias_type.to_s.should eq("alias MyAlias = Foo")
+    alias_type.generate.should eq(expected)
+    alias_type.to_s.should eq(expected)
   end
 
   it "creates an alias with unions" do
-    alias_type_generate = Crygen::Types::Alias.new("MyAlias", %w[Foo Bar])
-    alias_type_generate.generate.should eq("alias MyAlias = Foo | Bar")
-    alias_type_generate.to_s.should eq("alias MyAlias = Foo | Bar")
+    expected = <<-CRYSTAL
+    alias MyAlias = Foo | Bar
+    CRYSTAL
 
-    alias_type_to_s = Crygen::Types::Alias.new("MyAlias", %w[A::Foo B::Foo C::Bar D::Bar])
-    alias_type_to_s.generate.should eq("alias MyAlias = A::Foo | B::Foo | C::Bar | D::Bar")
-    alias_type_to_s.to_s.should eq("alias MyAlias = A::Foo | B::Foo | C::Bar | D::Bar")
+    alias_type_generate = Crygen::Types::Alias.new("MyAlias", %w[Foo Bar])
+    alias_type_generate.generate.should eq(expected)
+    alias_type_generate.to_s.should eq(expected)
+
+    expected = <<-CRYSTAL
+    alias MyAlias = A::Foo | B::Foo | C::Bar | D::Bar
+    CRYSTAL
+
+    alias_type_union = Crygen::Types::Alias.new("MyAlias", %w[A::Foo B::Foo C::Bar D::Bar])
+    alias_type_union.generate.should eq(expected)
+    alias_type_union.to_s.should eq(expected)
+  end
+
+  it "creates an alias with comment" do
+    expected = <<-CRYSTAL
+    # This is my alias
+    alias MyAlias = Foo
+    CRYSTAL
+
+    alias_type = Crygen::Types::Alias.new("MyAlias", ["Foo"])
+    alias_type.add_comment("This is my alias")
+    alias_type.generate.should eq(expected)
+    alias_type.to_s.should eq(expected)
   end
 end
