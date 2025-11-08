391171541d8f21f583d5bf37f649a84d08a4b876
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-11-08 13:09:26 +0100

    Add examples into unit tests

--- spec/class/class_class_vars_spec.cr
+++ spec/class/class_class_vars_spec.cr
@@ -40,6 +40,47 @@ describe "(class vars only)" do
     class_type.to_s.should eq(expected)
   end
 
+  it "creates a class with many class vars (annotations only)" do
+    expected = <<-CRYSTAL
+    class Person
+      @[Information]
+      @@first_name : String
+
+      @[Information]
+      @@last_name : String
+    end
+    CRYSTAL
+
+    the_annotation = CGT::Annotation.new("Information")
+
+    class_type = test_person_class()
+    class_type.add_class_var("first_name", "String", the_annotation: the_annotation)
+    class_type.add_class_var("last_name", "String", the_annotation: the_annotation)
+    class_type.generate.should eq(expected)
+    class_type.to_s.should eq(expected)
+  end
+
+  it "creates a class with many class vars (one annotation)" do
+    expected = <<-CRYSTAL
+    class Person
+      @@age : Int32
+      @@first_name : String
+
+      @[Information]
+      @@last_name : String
+    end
+    CRYSTAL
+
+    the_annotation = CGT::Annotation.new("Information")
+
+    class_type = test_person_class()
+    class_type.add_class_var("age", "Int32")
+    class_type.add_class_var("first_name", "String")
+    class_type.add_class_var("last_name", "String", the_annotation: the_annotation)
+    class_type.generate.should eq(expected)
+    class_type.to_s.should eq(expected)
+  end
+
   it "creates a class with many class vars with one default value" do
     expected = <<-CRYSTAL
     class Person
--- spec/struct/struct_class_vars_spec.cr
+++ spec/struct/struct_class_vars_spec.cr
@@ -40,6 +40,27 @@ describe "(class vars only)" do
     struct_type.to_s.should eq(expected)
   end
 
+  it "creates a struct with many class vars (annotations only)" do
+    expected = <<-CRYSTAL
+    struct Point
+      @@age : Int32
+      @@first_name : String
+
+      @[Information]
+      @@last_name : String
+    end
+    CRYSTAL
+
+    the_annotation = CGT::Annotation.new("Information")
+
+    struct_type = test_point_struct()
+    struct_type.add_class_var("age", "Int32")
+    struct_type.add_class_var("first_name", "String")
+    struct_type.add_class_var("last_name", "String", the_annotation: the_annotation)
+    struct_type.generate.should eq(expected)
+    struct_type.to_s.should eq(expected)
+  end
+
   it "creates a struct with many class vars with one default value" do
     expected = <<-CRYSTAL
     struct Point
