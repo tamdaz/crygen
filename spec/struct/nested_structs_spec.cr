297df46478ed78a1cda8ffa2d2ffe453284b31a4
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-11-08 21:38:16 +0100

    Add an example for testing the nested structs.

--- spec/struct/nested_structs_spec.cr
+++ spec/struct/nested_structs_spec.cr
@@ -72,4 +72,37 @@ describe Crygen::Types::Class do
         .add_struct(test_point_struct())
     ).to_s.should eq(expected)
   end
+
+  it "create a recursive struct with a method & comments." do
+    expected = <<-CRYSTAL
+    # Point struct
+    struct Point
+      # value method
+      def value : Nil
+      end
+
+      # Another Point struct
+      struct Point
+        # value method
+        def value : Nil
+        end
+      end
+    end
+    CRYSTAL
+
+    point_struct = test_point_struct()
+    point_struct.add_comment("Point struct")
+    point_struct.add_method(
+      Crygen::Types::Method.new("value", "Nil").add_comment("value method")
+    )
+
+    another_point_struct = test_point_struct()
+    another_point_struct.add_comment("Another Point struct")
+    another_point_struct.add_method(
+      Crygen::Types::Method.new("value", "Nil").add_comment("value method")
+    )
+
+    point_struct.add_struct(another_point_struct)
+    point_struct.to_s.should eq(expected)
+  end
 end
