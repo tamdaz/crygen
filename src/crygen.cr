2853989d7892116924429c1a00dd7b23462d7f7a
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-12-29 22:42:10 +0100

    Add an alias for the interface.

--- src/crygen.cr
+++ src/crygen.cr
@@ -17,4 +17,7 @@ module Crygen
 
   # CGG is an alias of "**C**ry**G**en **G**enerators".
   alias ::CGG = Crygen::Generators
+
+  # CGG is an alias of "**C**ry**G**en **I**nterfaces".
+  alias ::CGI = Crygen::Interfaces
 end
